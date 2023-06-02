b069784cfe5a9e46d2364dac076e4e3015c46188
JC (Jonathan Chen) <jc@dijonkitchen.org>
2023-06-02 10:46:19 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@
 # In your `$HOME` directory,
 # symbolic link these files:
 cd "$HOME" || exit
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc .
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig .
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.gitignore_global .
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.gitmessage .
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc .
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig .
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitignore_global .
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitmessage .
 
 cd -
