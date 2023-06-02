073b09ce17f317f0e5d2e31cd589b96defdbe6a5
JC (Jonathan Chen) <jc@dijonkitchen.org>
2023-06-02 10:34:01 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# In your `$HOME` directory,
+# symbolic link these files:
+cd "$HOME" || exit
+ln -si ./.bashrc .
+
+# Optionally link `.gitconfig`
+# or use your own credentials.
+ln -s ./.gitconfig .
+ln -s ./.gitignore_global .
+ln -s ./.gitmessage .
+
+cd -
