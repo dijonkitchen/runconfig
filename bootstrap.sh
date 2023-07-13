89e8557d0dab34ac3f0db8a07ba3988597889cb1
JC (Jonathan Chen) <jc@dijonkitchen.org>
2023-07-13 09:44:59 -0400

    build: remove environment specific things

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 # In your `$HOME` directory,
 # symbolic link these files:
 cd "$HOME" || exit
-ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc .
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig .
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitignore_global .
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitmessage .
