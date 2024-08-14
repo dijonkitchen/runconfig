514f6f9a455987734b236179a8c3a5fe25502218
JC (Jonathan Chen) <jc@dijonkitchen.org>
2024-08-14 09:27:55 -0400

    build: simplify and add .zshrc too

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 # For faster typing
+# Need to append to Codespaces defaults to get their benefits
 echo 'alias ls="ls -la --color=auto"' >> ~/.bashrc
 echo 'alias g="git"' >> ~/.bashrc
+
+echo 'alias ls="ls -la --color=auto"' >> ~/.zshrc
+echo 'alias g="git"' >> ~/.zshrc
 
 # In your `$HOME` directory,
 # symbolic link these files:
 cd "$HOME" || exit
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig .
-ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitignore_global .
-ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.gitmessage .
 
 cd -
